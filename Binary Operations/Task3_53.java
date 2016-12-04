669c5c58f95fd8664691e5563d436dd24cddcf35
ivanmarkov13 <ivan.markov13@yahoo.com>
2016-12-04 13:14:19 +0200

    Add files via upload

---
+++ Binary Operations/Task3_53.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+public class Task3_53 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		int counter = 0;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		counter += num % 2;
+		num /= 2;
+		
+		System.out.println(counter + " bits in the number are 1");
+
+	}
+
+}
